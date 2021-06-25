0e31c35a8986c42df2603637ffbd9bcdcc99d862
youngchief btw ツ <youngchief@youngchief.tk>
2021-06-25 19:40:22 +0000

    Fix up the main header file + add test.cpp

---
+++ src/CryptoChief.h
@@ -0,0 +1,11 @@
+#ifdef __cplusplus /* If this is a C++ compiler, use C linkage */
+extern "C" {
+#endif
+
+namespace CryptoChief {
+    #include "hash.h"
+}
+
+#ifdef __cplusplus /* If this is a C++ compiler, end C linkage */
+}
+#endif
---
+++ src/test.cpp
@@ -0,0 +1,6 @@
+#include "CryptoChief.h"
+using namespace std;
+
+int main() {
+    CryptoChief::hash(["hi"], ["SHA3-512"]);
+}
